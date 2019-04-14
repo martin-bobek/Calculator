fca5e067e1db168be8acf5a068f597c7e82ec3bb
martin-bobek <martinbobek8@gmail.com>
2019-04-14 15:10:21 -0600

    Started implementing the ViewModel.
    Added a generic OnPropertyChanged method which raises the
    PropertyChanged event and checks that a valid proprty name was used
    (only in Debug mode). Added a Display property which will represent the
    calculator's display.
    
    	new file:   ViewModel/ViewModel.cs

---
+++ ViewModel/ViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+namespace Calculator.ViewModel
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string displayString;
+
+        public string Display
+        {
+            get { return displayString; }
+            private set
+            {
+                if (displayString == value)
+                    return;
+
+                displayString = value;
+                OnPropertyChanged("Display");
+            }
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            VerifyPropertyName(propertyName);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        [Conditional("DEBUG")]
+        private void VerifyPropertyName(string propertyName)
+        {
+            if (TypeDescriptor.GetProperties(this)[propertyName] == null)
+                Debug.Fail("Property does not exist: " + propertyName);
+        }
+    }
+}
