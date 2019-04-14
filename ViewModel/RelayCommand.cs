c234ff3fd78cc6ed20a7754405fb27ed4dec2078
martin-bobek <martinbobek8@gmail.com>
2019-04-14 14:46:27 -0600

    Added an implementation of ICommand.
    Added an ICommand class which will be used to relay commands from button
    presses in the View. The class supports only commands for which
    CanExecute is always true and the command does not accept any
    parameters.
    
    	new file:   ViewModel/RelayCommand.cs

---
+++ ViewModel/RelayCommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Calculator.ViewModel
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action execute;
+
+        public RelayCommand(Action execute)
+        {
+            this.execute = execute ?? throw new ArgumentNullException("execute");
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { }
+            remove { }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public void Execute(object parameter)
+        {
+            execute();
+        }
+    }
+}
