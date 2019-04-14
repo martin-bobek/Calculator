bd06fa0644d2957872ecb31bc69f47156fe55152
martin-bobek <martinbobek8@gmail.com>
2019-04-14 01:34:08 -0600

    Wrote starting point for model class.
    Basic calculator operations are supported using an accumulator and a set
    of functions which perform operations on the accumulator.

---
+++ Model/Model.cs
@@ -0,0 +1,28 @@
+﻿namespace Calculator.Model
+{
+    public class Model
+    {
+        public double Accumulator { get; private set; }
+
+        public void ClearAccumulator()
+        {
+            Accumulator = 0;
+        }
+        public void Add(double value)
+        {
+            Accumulator += value;
+        }
+        public void Subtract(double value)
+        {
+            Accumulator -= value;
+        }
+        public void Multiply(double value)
+        {
+            Accumulator *= value;
+        }
+        public void Divide(double value)
+        {
+            Accumulator /= value;
+        }
+    }
+}
