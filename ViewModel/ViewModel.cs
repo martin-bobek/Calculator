a867b23e1192cb5d625e04742b0052df3ca3a072
martin-bobek <martinbobek8@gmail.com>
2019-04-14 20:15:52 -0600

    Bound operation button clicks to ViewModel.
    Operation buttons are bound to OpCommands in ViewModel. Pressing a
    button calls OnOperationCommand, which is passed an enum representing
    the type of operation.
    
    	modified:   View/View.xaml
    	modified:   ViewModel/ViewModel.cs

--- ViewModel/ViewModel.cs
+++ ViewModel/ViewModel.cs
@@ -6,21 +6,26 @@ using System.Windows.Input;
 
 namespace Calculator.ViewModel
 {
+    public enum Operation { Add, Sub, Mult, Div, Neg, Equals, Last = Equals };
+
     public class ViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
         private string displayString;
         private readonly ReadOnlyCollection<ICommand> numCommands;
+        private readonly ReadOnlyDictionary<Operation, ICommand> opCommands;
+        private Operation current = Operation.Add;
         private readonly Model.Model model;
 
         public ViewModel()
         {
             model = new Model.Model();
-            CreateCommands(out numCommands);
+            CreateCommands(out numCommands, out opCommands);
         }
 
         public string Display
         {
+            // TODO - Prevent display from overfilling
             get { return displayString; }
             private set
             {
@@ -35,15 +40,46 @@ namespace Calculator.ViewModel
         {
             get { return numCommands; }
         }
+        public ReadOnlyDictionary<Operation, ICommand> OpCommands
+        {
+            get { return opCommands; }
+        }
 
         private void OnNumberCommand(int num)
         {
             // TODO - implement number entry logic here
             Display += num;
         }
-        private void CreateCommands(out ReadOnlyCollection<ICommand> numCommands)
+        private void OnOperationCommand(Operation op)
+        {
+            // TODO - Implement
+            switch (op)
+            {
+                case Operation.Add:
+                    Display = "Add";
+                    break;
+                case Operation.Sub:
+                    Display = "Subtract";
+                    break;
+                case Operation.Mult:
+                    Display = "Multiply";
+                    break;
+                case Operation.Div:
+                    Display = "Divide";
+                    break;
+                case Operation.Neg:
+                    Display = "Negate";
+                    break;
+                case Operation.Equals:
+                    Display = "Equals";
+                    break;
+            }
+        }
+        private void CreateCommands(out ReadOnlyCollection<ICommand> numCommands,
+                                    out ReadOnlyDictionary<Operation, ICommand> opCommands)
         {
             numCommands = new ReadOnlyCollection<ICommand>(CreateNumCommands());
+            opCommands = new ReadOnlyDictionary<Operation, ICommand>(CreateOpCommands());
         }
         private List<ICommand> CreateNumCommands()
         {
@@ -52,6 +88,16 @@ namespace Calculator.ViewModel
             {
                 int captured = num;
                 commands.Add(new RelayCommand(() => OnNumberCommand(captured)));
+            }
+            return commands;
+        }
+        private Dictionary<Operation, ICommand> CreateOpCommands()
+        {
+            var commands = new Dictionary<Operation, ICommand>();
+            for (Operation op = 0; op <= Operation.Last; op++)
+            {
+                Operation captured = op;
+                commands.Add(captured, new RelayCommand(() => OnOperationCommand(captured)));
             }
             return commands;
         }
