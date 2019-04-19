15fa6ab636825185d5c63ed7361c52c88a4d9dbc
martin-bobek <martinbobek8@gmail.com>
2019-04-19 16:00:54 -0600

    Added test project.
    Added a project called CalculatorTests which will contain all unit tests
    for the project Calculator.
    
    	new file:   CalculatorTests/Properties/AssemblyInfo.cs
    	new file:   CalculatorTests/packages.config

---
+++ CalculatorTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalculatorTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalculatorTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("532f6464-de72-4242-8921-a28d4cbac18a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
